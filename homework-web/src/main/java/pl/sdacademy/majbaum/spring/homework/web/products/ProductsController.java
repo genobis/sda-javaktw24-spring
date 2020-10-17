2e93030bdb6eba1555d466a5fcb37801be4a1824
Tomasz Majbaum <tomasz.majbaum@gmail.com>
2020-10-17 11:04:52 +0200

    homework-web: Drobne poprawki zadania domowego

--- homework-web/src/main/java/pl/sdacademy/majbaum/spring/homework/web/products/ProductsController.java
+++ homework-web/src/main/java/pl/sdacademy/majbaum/spring/homework/web/products/ProductsController.java
@@ -2,7 +2,6 @@ package pl.sdacademy.majbaum.spring.homework.web.products;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.ModelAndView;
