79dfdb7a601f798a280959f6b4c436c47e3cd993
Tomasz Majbaum <tomasz.majbaum@gmail.com>
2020-10-24 13:13:24 +0200

    spring-security: UserDetailsServiceImpl

---
+++ spring-security/src/main/java/pl/sdacademy/majbaum/spring/security/auth/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package pl.sdacademy.majbaum.spring.security.auth;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import pl.sdacademy.majbaum.spring.security.UserRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findById(username)
+                .map(UserDetailsImpl::new)
+                .orElseThrow(() -> new UsernameNotFoundException(username));
+    }
+}
