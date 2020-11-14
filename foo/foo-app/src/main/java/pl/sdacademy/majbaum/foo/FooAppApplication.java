aaebb49963292d6d5a6dca0ffe258604c1996e35
Tomasz Majbaum <tomasz.majbaum@gmail.com>
2020-11-14 13:10:26 +0100

    Przykładowa aplikacja modułowa

---
+++ foo/foo-app/src/main/java/pl/sdacademy/majbaum/foo/FooAppApplication.java
@@ -0,0 +1,13 @@
+package pl.sdacademy.majbaum.foo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FooAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FooAppApplication.class, args);
+	}
+
+}
